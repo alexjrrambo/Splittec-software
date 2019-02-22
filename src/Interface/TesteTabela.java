b8e69160d11e3a3a36920c3a7e6fed8e049a48b0
alexjrrambo <44339856+alexjrrambo@users.noreply.github.com>
2019-02-22 13:31:17 -0300

    Update TesteTabela.java

--- src/Interface/TesteTabela.java
+++ src/Interface/TesteTabela.java
@@ -62,10 +62,10 @@ import java.awt.event.MouseEvent;
  */
 
 /**
- *  @author Elvis Dobner da Rosa
+ *  @author Alex Rambo
  *  @since 10/02/2011
  *  
- *  Classe demosntraÁ„o para construÁ„o de janela (tipo JFrame).
+ *  Classe demosntra√ß√£o para constru√ß√£o de janela (tipo JFrame).
  */
 public class TesteTabela extends JFrame {
 	private JTextField jTextField8;
@@ -73,7 +73,7 @@ public class TesteTabela extends JFrame {
 
 	/**
 	 *  TODO Construtor da classe
-	 *  O construtor È invocado quando a classe È criada!
+	 *  O construtor √© invocado quando a classe √© criada!
 	 */
 	public TesteTabela(){
 		super();
@@ -82,17 +82,17 @@ public class TesteTabela extends JFrame {
 	}
 
 	/**
-	 *  TODO MÈtodo respons·vel por construir a interface da janela.
+	 *  TODO M√©todo respons√°vel por construir a interface da janela.
 	 */
 	public void  initGUI(){
 
 		//Coloca mensagem na barra do programa
 		this.setTitle("Cadastro Itens de Movimento");
 
-		//Permite Maximizat ou n„o true (sim) false (n„o)
+		//Permite Maximizat ou n√£o true (sim) false (n√£o)
 		this.setResizable(false);
 
-		//Define a operaÁ„o padr„o ao clicar no bot„o 'close'
+		//Define a opera√ß√£o padr√£o ao clicar no bot√£o 'close'
 		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		//Serve para desenhar dentro do Panel
 		this.pack();
@@ -100,7 +100,7 @@ public class TesteTabela extends JFrame {
 		//Definindo o tamanho da janela
 		this.setSize(625, 250);
 
-		//Obtendo resoluÁ„o da tela
+		//Obtendo resolu√ß√£o da tela
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 
 		//Centralizando janela na tela obtida

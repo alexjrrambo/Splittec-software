c85c3224fd0994c72a5e7d8c73bfa6351820f2c7
alexjrrambo <44339856+alexjrrambo@users.noreply.github.com>
2019-02-22 13:26:49 -0300

    Update MovimentoItemAction.java

--- src/Action/MovimentoItemAction.java
+++ src/Action/MovimentoItemAction.java
@@ -33,26 +33,20 @@ public class MovimentoItemAction {
 	public static JTableList tableList;
 
 	/**
-	 *  TODO Atualiza a lista na grid!
+	 * Atualiza a lista na grid!
 	 */
 	public static void doReload(){
 		tableList.doReload();
 	}
 
-	/**
-	 *  TODO O mÈtodo È invocado quando o bot„o 'Pesquisar' (Lupa) È clicado!
-	 *  Passo1 - Receber um objeto com os critÈrios de busca;
-	 *  Passo2 - Executar o SQL para receber o resultado da busca (lista);
-	 *  Passo2 - Retornar a lista para que a Grid seja atualizada.
-	 */
 	public static ArrayList<MovimentoItem> doLoadList(MovimentoItem objeto){
 
 		ArrayList<MovimentoItem> lista = new ArrayList<MovimentoItem>();
 
-		//SeleÁ„o de Registros
+		//Sele√ß√£o de Registros
 		String querySelect = "SELECT mi.*, ma.descricao,ma.unidade_medida,ma.quantidade as qtd_material FROM Movimento_item mi left join material ma on ma.id = mi.id_material ";
 
-		//--Realizar tratamento (Verificar se È WHERE ou AND)..
+		//--Realizar tratamento (Verificar se √© WHERE ou AND)..
 		boolean isWhere = true;
 		if(objeto.getId_movimento()!=null){
 			if(isWhere){
@@ -108,7 +102,7 @@ public class MovimentoItemAction {
 
 
 	/**
-	 *  TODO MÈtodo respons·vel por configurar a grid da tela de pesquisa! 
+	 * M√©todo respons√°vel por configurar a grid da tela de pesquisa! 
 	 */
 	public void createTable(Window window, JPanel panel){
 
@@ -143,23 +137,23 @@ public class MovimentoItemAction {
 		Font fonte = new Font("Times New Roman", Font.ITALIC, 16);
 		tableList.setTitleFont(fonte);
 
-		//Adicionando cabeÁalho com tÌtulo e imagem
+		//Adicionando cabe√ßalho com t√≠tulo e imagem
 		tableList.setTitle("Materiais do movimento",ImageUtils.resizeImageIcon("Imagens/canstock11432632.png",32,32));
 
 
-		//Configurando mÈtodo para adiÁ„o de registros
+		//Configurando m√©todo para adi√ß√£o de registros
 		tableList.setNewMethod("doNew");
 		
 
-		//Configurando mÈtodo para impress„o de relatÛrios
+		//Configurando m√©todo para impress√£o de relat√≥rios
 		tableList.setReportMethod("doPrintReport");
 
-		//Adicionando bot„o para visualizaÁ„o
+		//Adicionando bot√£o para visualiza√ß√£o
 		ImageIcon imageOpen = new ImageIcon(getClass().getClassLoader().getResource("Framework/Imagens/abrir.png"));
 		tableList.addButton(imageOpen, "doLoad", "Visualizar");
 
 		
-		//Adicionando bot„o para exclus„o
+		//Adicionando bot√£o para exclus√£o
 		ImageIcon imageExcluir = new ImageIcon(getClass().getClassLoader().getResource("Framework/Imagens/excluir.png"));
 		tableList.addButton(imageExcluir, "doRemove", "Excluir");
 
@@ -170,12 +164,12 @@ public class MovimentoItemAction {
 
 
 	/**
-	 *  MÈtodo para adicionar registros
+	 *  M√©todo para adicionar registros
 	 */
 	public static void doNew(){		
 		String valida = CadastroMovimento.textFieldCodigo.getText();
 		if(!valida.equals("")){
-			JOptionPane.showMessageDialog(null,"N„o e possivel adicionar materiais em um movimento que ja foi salvo anteriormente.");
+			JOptionPane.showMessageDialog(null,"N√£o e possivel adicionar materiais em um movimento que ja foi salvo anteriormente.");
 		}else{
 			CadastroMovimentoItem cadastro = new CadastroMovimentoItem();
 			cadastro.setaIdMovimento();
@@ -184,7 +178,7 @@ public class MovimentoItemAction {
 	}
 
 	/**
-	 *  MÈtodo para visualizar registros
+	 *  M√©todo para visualizar registros
 	 */
 	public static void doLoad(MovimentoItem MovimentoItem){
 
@@ -194,18 +188,18 @@ public class MovimentoItemAction {
 	}
 
 	/**
-	 *  MÈtodo para remover registros
+	 *  M√©todo para remover registros
 	 */
 	public static void doRemove(MovimentoItem objeto){
 
 		if(!CadastroMovimento.textFieldCodigo.getText().equals("")){
-			JOptionPane.showMessageDialog(null,"N„o e possivel excluir materiais de um movimento que ja foi salvo anteriormente.");
+			JOptionPane.showMessageDialog(null,"N√£o e possivel excluir materiais de um movimento que ja foi salvo anteriormente.");
 		}else{
-			int option = JOptionPane.showConfirmDialog(null,"Realmente deseja excluir?","AtenÁ„o!",1 );
+			int option = JOptionPane.showConfirmDialog(null,"Realmente deseja excluir?","Aten√ß√£o!",1 );
 
 			if(option == 0){
 
-				//RemoÁ„o de Registros
+				//Remo√ß√£o de Registros
 				String queryDelete = "DELETE FROM Movimento_item WHERE id = "+objeto.getId();
 				ConnectionUtils.executeUpdate(queryDelete);
 
@@ -216,7 +210,7 @@ public class MovimentoItemAction {
 	}
 
 	/**
-	 *  MÈtodo para imprimir relatÛrios!
+	 *  M√©todo para imprimir relat√≥rios!
 	 *  Somente o que aparece na grid deve ser impresso.
 	 */
 	public static void doPrintReport(){
